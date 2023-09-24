ee39821054a9a071767ee270d66c7950fe412a56
FedorEvgenievich <Boss.gooog@gmail.com>
2023-09-24 17:12:13 +0300

    add filter, scss file products

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const list = document.querySelector('.products__list'),
+  items = document.querySelectorAll('.products__item'),
+  listitems = document.querySelectorAll('.products__list-item')
+
+function filter() {
+  list.addEventListener('click', event => {
+    const targetId = event.target.dataset.id
+    const target = event.target
+
+    if(target.classList.contains('products__list-item')){
+      listitems.forEach(listItem => listItem.classList.remove('active'))
+      target.classList.add('active')
+    }
+
+    switch(targetId){
+      case 'sand':
+        getItems(targetId)
+        break
+
+        case 'stone':
+        getItems(targetId)
+        break
+        
+        case 'aspchalt':
+        getItems(targetId)
+        break
+    }
+  })
+}
+filter()
+
+function getItems(className){
+  items.forEach(item => {
+    if (item.classList.contains(className)) {
+      item.style.display = 'block'
+    } else {
+      item.style.display = 'none'
+    }
+  })
+}
